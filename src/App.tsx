c162461c1e37b5c8bb78cf7bb22d5d91a10c2361
José Lazaretti <eduardo1793@gmail.com>
2022-07-16 15:16:42 -0300

    Organazing main and app

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,14 @@
-import { AppstoreOutlined, MailOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
-import 'antd/dist/antd.css';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+import { LightTheme } from "./shared/themes";
 
-const getItem = (label?, key?, icon?, children?, type?) => {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  };
-}
-
-const items = [
-  getItem('Planning Poker', 'sub1', <MailOutlined />, [
-    getItem('Text', '1'),
-    getItem(<Link to="pointing-poker">Tool</Link>, '2'),
-  ]),
-  getItem('Retrospective', 'sub2', <AppstoreOutlined />, [
-    getItem('Text', '5'),
-    getItem(<Link to="retrospective">Tool</Link>, '6'),
-    getItem('Examples', 'sub3', null, [
-      getItem('Example 1', '7'),
-      getItem('Example 2', '8')
-    ]),
-  ]),
-]; // submenu keys of first level
-
-const rootSubmenuKeys = ['sub1', 'sub2'];
-
-const handleClick = (info) => {
-  switch (info.key) {
-    case 1:
-      useHistory
-      break;
-  
-    default:
-      break;
-  }
-  console.log(`clicked ${info.key}`);
-  console.log(info);
-}
-
-const App = () => {
-  const [openKeys, setOpenKeys] = useState(['sub1']);
-
-  const onOpenChange = (keys) => {
-    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
-
-    if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-      setOpenKeys(keys);
-    } else {
-      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
-    }
-  };
-
+export const App = () => {
   return (
-    <Menu
-      theme='dark'
-      mode="inline"
-      openKeys={openKeys}
-      onOpenChange={onOpenChange}
-      style={{
-        width: 256,
-      }}
-      items={items}
-      onClick={handleClick}
-    >
-    </Menu>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
-};
-
-export default App
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
-import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter
-} from "react-router-dom";
-import "./main.css";
-import { AppRoutes } from "./routes";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-);
-root.render(
-  <BrowserRouter>
-    <AppRoutes />
-  </BrowserRouter>
+import { App } from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
